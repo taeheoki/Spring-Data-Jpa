f54fddefa0976055a72e7509d1bca07f984ae0fa
taeheoki <dkqjwl2365@naver.com>
2023-07-12 16:33:52 +0900

    🧑🏻‍💻feat: 공통 인터페이스 설정

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -20,6 +20,7 @@ class MemberRepositoryTest {
 
     @Test
     public void testMember() {
+        System.out.println(memberRepository.getClass());
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
