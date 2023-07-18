d6de7b5a1918255a05d1b9c8cad82b350e60e315
taeheoki <dkqjwl2365@naver.com>
2023-07-18 17:04:37 +0900

    🧑🏻‍💻feat: 사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -301,4 +301,9 @@ class MemberRepositoryTest {
         // when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() throws Exception {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
